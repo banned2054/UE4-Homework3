cba80eabedfa2e4e4e99f93a88a52d402630c1cc
banned2054 <34962009+banned2054@users.noreply.github.com>
2021-12-18 18:17:03 +0800

    1.0

--- Source/MultPlayerServer.Target.cs
+++ Source/MultPlayerServer.Target.cs
@@ -1,13 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class MultPlayerServerTarget : TargetRules
 {
-
-    public MultPlayerServerTarget(TargetInfo Target) : base(Target)
-    {
-        Type = TargetType.Server;
-        DefaultBuildSettings = BuildSettingsVersion.V2;
-        ExtraModuleNames.Add("MultPlayer"); //根据你的项目名称更改此行
-    }
+	public MultPlayerServerTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Server;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("MultPlayer");
+	}
 }
